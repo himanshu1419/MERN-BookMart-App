888f3214894c71fea7baea179d915452eb8dfd92
himanshunegi1419 <himanshunegi1419@gmail.com>
2025-01-19 20:42:48 +0530

    Adding user routes

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const port = process.env.PORT || 5000;
+
+require('dotenv').config()
+
+// middleware
+app.use(express.json());
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://book-app-frontend-tau.vercel.app'],
+    credentials: true
+}))
+
+// routes
+
+const userRoutes =  require("./src/users/user.route")
+
+
+
+app.use("/api/auth", userRoutes)
+
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_URI);
+  app.use("/", (req, res) => {
+    res.send("Book Store Server is running!");
+  });
+}
+
+main().then(() => console.log("Mongodb connect successfully!")).catch(err => console.log(err));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+  
+});
