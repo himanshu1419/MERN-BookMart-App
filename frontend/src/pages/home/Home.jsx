83e13261e16109d06601cb2c471ebcb8d53839a2
himanshunegi1419 <himanshunegi1419@gmail.com>
2025-01-23 20:16:45 +0530

    Created TopSellers and Recommend section components

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Banner from './Banner'
+import TopSellers from './TopSellers'
+import Recommend from './Recommend'
+import News from './News'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+        <Banner/>
+        <TopSellers/>
+        <Recommend/>
+        <News/>
+    </>
   )
 }
 
